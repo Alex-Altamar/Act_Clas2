4cdee126b10605dc5f096dd851100c25864405c8
Alex-Altamar <aealtamar@uniguajira.edu.co>
2022-03-10 18:23:36 -0500

    adiciono primera parte de los diccionarios

--- Ejercicio4.py
+++ Ejercicio4.py
@@ -1,8 +1,23 @@
 from api.library import *
 
 def main ():
-   #Aplicacion de los diccionarios
+   #Aplicacion de los diccionarios estos tienen propiedades y valores
+   """
+   persona = {
+      "Nombre": "Alexander",
+      "Apellidos": "Altamar Vanegas",
+      "edad": 25
    
+   """
+   persona = {
+      "Nombre": "Alexander",
+      "Apellidos": "Altamar Vanegas",
+      "edad": 25
+   }
+
+   #print(persona)
+   print(f"{persona['Nombre']}  {persona['Apellidos']}") #muestra elementos especificos del diccionario
+
 
 
 if __name__ == "__main__":
